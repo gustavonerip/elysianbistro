01c0ac1e707e47c6fd450b88aafed55bdaa2eccc
Gustavo <gustavoneripeixoto@gmail.com>
2024-04-17 15:06:00 -0700

    gsap animations added to pages

--- public/js/main.js
+++ public/js/main.js
@@ -26,4 +26,50 @@ window.addEventListener("load", () => {
       cursor.style.top = `${y}px`;
     }, delay);
   });
+
+  gsap.defaults({
+    ease: "power3.out",
+  });
+
+  let tl = gsap.timeline();
+
+  tl.fromTo(
+    ".logo h1",
+    { scaleY: 0, opacity: 0, transformOrigin: "bottom", bottom: "-150px" },
+    { scaleY: 1, opacity: 1, bottom: 0, duration: 1 }
+  );
+
+  tl.fromTo(
+    ".logo h1 .star",
+    { scale: 0, transformOrigin: "center" },
+    { scale: 1, delay: 0.1, stagger: 0.3 }
+  );
+
+  tl.fromTo(".logo img", { opacity: 0 }, { opacity: 1, duration: 1.5 }, 1);
+
+  tl.fromTo(
+    ".content p",
+    {
+      y: "200%",
+      opacity: 0,
+    },
+    { y: "0%", opacity: 1, duration: 1.5, ease: "power4.out" },
+    "-=1"
+  );
+
+  tl.fromTo(
+    ".content .cta-wrapper a",
+    {
+      x: "100%",
+      opacity: 0,
+    },
+    {
+      x: "0%",
+      opacity: 1,
+      duration: 0.5,
+      ease: "power3.out",
+      stagger: 0.1,
+    },
+    "-=1.5"
+  );
 });
